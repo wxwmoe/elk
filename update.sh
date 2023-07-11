8e7e613c325cea8655efb0dcc896a1e88168a8a9
FGHRSH <fghrsh@wxw.moe>
2023-07-11 09:06:25 +0800

    add notes

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 git reset --hard
 git pull
 git checkout release
+# set elk open graph
 sed -i "s|'Elk'|'Elk (wxw.moe)'|" nuxt.config.ts
 sed -i "s|elk\.zone|elk.wxw.moe|" nuxt.config.ts
+# set elk default server
 sed -i "s|m\.webtoo\.ls|wxw.moe|" nuxt.config.ts
+# set elk oauth app info
 sed -i "s|elk\.zone|elk.wxw.moe|" server/utils/shared.ts
 sed -i "/client_name/{s|''|' (wxw.moe)'|}" server/utils/shared.ts
